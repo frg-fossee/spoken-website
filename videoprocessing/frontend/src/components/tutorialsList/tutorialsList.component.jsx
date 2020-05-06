a75dfe04e2d2a11a1a6b66c8457f1a40356e7863
Pratik Daigavane <daigavanep@gmail.com>
2020-05-06 23:41:31 +0530

    dropdown complete, table started

--- videoprocessing/frontend/src/components/tutorialsList/tutorialsList.component.jsx
+++ videoprocessing/frontend/src/components/tutorialsList/tutorialsList.component.jsx
@@ -1,20 +1,74 @@
 import React from "react";
 import axios from 'axios'
-import {Col, Row, Select, Typography} from 'antd';
+import {Col, Row, Select, Table, Typography} from 'antd';
 
 const {Option} = Select;
 const {Text} = Typography
+const columns = [
+    {
+        title: 'Tutorial Name',
+        dataIndex: 'tutorial',
+        key: 'tutorial',
+    },
+    {
+        title: 'FOSS',
+        dataIndex: 'foss',
+        key: 'foss',
+    },
+    {
+        title: 'Language',
+        dataIndex: 'language',
+        key: 'language',
+    },
+];
 
 class TutorialsListComponent extends React.Component {
     constructor() {
         super();
         this.state = {
             tutorials: [],
-            allFosses: [],
-            allTutorials: [],
-            allLanguages: [],
-            isLoading: true
+            filteredTutorials: [],
+            tutorialsInTable: [],
+            isLoading: true,
+            isTutDisabled: true
         }
+        this.filterFosses = this.filterFosses.bind(this)
+        this.filterTutorials = this.filterTutorials.bind(this)
+        this.renderAllFosses = this.renderAllFosses.bind(this);
+
+    }
+
+
+    async filterFosses(value, option) {
+        if (value === 'All'){
+            let all = this.state.tutorials
+
+            this.setState({filteredTutorials: all, isTutDisabled: false, tutorialsInTable: all})
+        }
+        let tuts = this.state.tutorials
+        tuts = await tuts.filter((item) => {
+            return item.foss === value
+        })
+        console.log(tuts)
+        this.setState({filteredTutorials: tuts, isTutDisabled: false, tutorialsInTable: tuts})
+    }
+
+    async filterTutorials(value, option){
+        let tuts = this.state.filteredTutorials
+        tuts = await tuts.filter((item) => {
+            return item.tutorial === value
+        })
+        console.log(tuts)
+        this.setState({tutorialsInTable: tuts})
+    }
+
+
+    renderAllFosses() {
+        let options = new Set()
+        this.state.tutorials.map((item) => {
+            options.add(<Option value={item.foss}>{item.foss}</Option>)
+        })
+        return options
     }
 
     componentDidMount() {
@@ -55,31 +109,34 @@ class TutorialsListComponent extends React.Component {
 
                 })
                 .then(() => {
-                    this.setState({tutorials: filtered_tuts,
+                    this.setState({
+                        tutorials: filtered_tuts,
                         isLoading: false,
-                        allTutorials: Array.from(tutorials),
-                        allFosses: Array.from(fosses)
-                        , allLanguages: Array.from(langs)})
+                        filteredTutorials: filtered_tuts,
+                        tutorialsInTable: filtered_tuts
+                    })
+
+
                 })
         })
     }
 
     render() {
+
         return (
+            <div>
+
             <Row justify="start" gutter={[24, 8]}>
-                <Col span={2} offset={6}>
-                    <Text>
-                        FOSS
-                    </Text>
+                <Col span={2} offset={6}> <Text>
+                    FOSS
+                </Text>
                 </Col>
                 <Col span={2}>
-
-                    <Select defaultValue="All" style={{width: 120}}>
+                    <Select onChange={this.filterFosses} defaultValue="All" style={{width: 120}}>
                         <Option value='All'>All</Option>
-                        {this.state.allFosses.map((foss)=>{
-                            return <Option value={foss}>{foss}</Option>
-                        })}
-
+                        {
+                            this.renderAllFosses()
+                        }
                     </Select>
                 </Col>
                 <Col span={2}>
@@ -88,30 +145,22 @@ class TutorialsListComponent extends React.Component {
                     </Text>
                 </Col>
                 <Col span={2}>
-                    <Select defaultValue="All" style={{width: 120}}>
+                    <Select allowClear disabled={this.state.isTutDisabled} defaultValue="All" style={{width: 120}} onChange={this.filterTutorials}>
                         <Option value='All'>All</Option>
-                        {this.state.allTutorials.map((foss)=>{
-                            return <Option value={foss}>{foss}</Option>
-                        })}                    </Select>
-                </Col>
-                <Col span={2}>
-                    <Text>
-                        Language
-                    </Text>
-                </Col>
-                <Col span={2}>
+                        {
+                            this.state.filteredTutorials.map((item)=>{
+                                return <Option value={item.tutorial}>{item.tutorial}</Option>
+                            })
 
-                    <Select defaultValue="All" style={{width: 120}}>
-                        <Option value='All'>All</Option>
-                        {this.state.allLanguages.map((foss)=>{
-                            return <Option value={foss}>{foss}</Option>
-                        })}                    </Select>
+                        }
+                    </Select>
                 </Col>
-
-
             </Row>
+        <Table dataSource={this.state.tutorialsInTable} columns={columns} />;
+            </div>
 
-        );
+
+    );
     }
 }
 
