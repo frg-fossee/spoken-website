91dcd5f0906315edf55e149df901c06b693357e7
Pratik Daigavane <daigavanep@gmail.com>
2020-05-11 16:33:42 +0530

    dashboard table added

--- videoprocessing/frontend/src/pages/Dashboard/dashboard.page.jsx
+++ videoprocessing/frontend/src/pages/Dashboard/dashboard.page.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import {withRouter} from "react-router-dom";
 import qs from 'qs'
 import axios from 'axios'
-import {notification, Progress,Typography} from "antd";
+import {Button, Col, Modal, notification, Progress, Row, Space, Spin, Table, Typography} from "antd";
+import ReactAudioPlayer from 'react-audio-player';
+import ReactPlayer from "react-player";
+import {AudioOutlined, InboxOutlined, DownloadOutlined} from '@ant-design/icons'
+import {Upload, message} from 'antd';
+
+const {Dragger} = Upload;
+const {Title, Text} = Typography;
+
 
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            loading: true,
             id: '',
             current_count: '',
             chunks: [],
@@ -19,8 +28,89 @@ class Dashboard extends React.Component {
             total_count: '',
             processed_video: '',
             processed: false,
-            progress_status: ''
+            progress_status: '',
+            visible: false
         }
+        this.config = {
+            name: 'file',
+            multiple: true,
+            action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+            onChange(info) {
+                const {status} = info.file;
+                if (status !== 'uploading') {
+                    console.log(info.file, info.fileList);
+                }
+                if (status === 'done') {
+                    message.success(`${info.file.name} file uploaded successfully.`);
+                } else if (status === 'error') {
+                    message.error(`${info.file.name} file upload failed.`);
+                }
+            },
+        }
+        this.columns = [
+            {
+                title: 'Chunk No.',
+                dataIndex: 'chunk_no',
+                key: 'chunk_no',
+                width: '5%'
+
+            },
+            {
+                title: 'Audio',
+                dataIndex: 'audio_chunk',
+                key: 'audio_chunk',
+                render: (audio_chunk) => {
+                    return (<ReactAudioPlayer
+                        src={audio_chunk}
+                        controls
+                        controlsList="nodownload"
+                    />)
+                }
+            },
+            {
+                title: 'Start Time',
+                dataIndex: 'start_time',
+                key: 'start_time',
+            },
+            {
+                title: 'End Time',
+                dataIndex: 'end_time',
+                key: 'end_time',
+            },
+            {
+                title: 'Subtitle',
+                dataIndex: 'subtitle',
+                key: 'subtitle',
+                sorter: (a, b) => a.subtitle.localeCompare(b.subtitle),
+                sortDirections: ['descend', 'ascend']
+            },
+            {
+                title: 'Change Audio',
+                render: () => {
+                    return (<Button icon={<AudioOutlined/>} onClick={this.showModal}>Change Audio</Button>
+                    )
+                }
+            },
+        ];
+        this.showModal = () => {
+            this.setState({
+                visible: true,
+            });
+        };
+
+        this.handleOk = e => {
+            console.log(e);
+            this.setState({
+                visible: false,
+            });
+        };
+
+        this.handleCancel = e => {
+            console.log(e);
+            this.setState({
+                visible: false,
+            });
+        };
 
         this.fetchData = () => {
             this.setState({processed: false})
@@ -36,7 +126,8 @@ class Dashboard extends React.Component {
                             language: res.data.video_data.language,
                             foss: res.data.video_data.foss,
                             status: res.data.video_data.status,
-                            checksum: res.data.video_data.checksum
+                            checksum: res.data.video_data.checksum,
+                            loading: false
                         });
                         if (res.data.video_data.processed_video === null) {
                             this.setState(
@@ -86,11 +177,73 @@ class Dashboard extends React.Component {
         clearInterval(this.apiLoop)
     }
 
+
     render() {
-        return (<div>
-            <Progress type="circle" percent={parseInt((this.state.current_count / this.state.total_count) * 100)} status={this.state.progress_status}/>
-            <Typography.Title level={3}>Status: {this.state.status.toUpperCase()}</Typography.Title>
-        </div>)
+        let isLoading = this.state.loading
+        return (
+            <div>
+                {isLoading ?
+                    <Space size="middle">
+                        <Spin size="large"/>
+                    </Space> : <div>
+                        <Row align="middle">
+                            <Col span={4}
+                                 style={{display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}>
+                                <Progress type="circle"
+                                          percent={parseInt((this.state.current_count / this.state.total_count) * 100)}
+                                          status={this.state.progress_status}/>
+                            </Col>
+                            <Col style={{display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}
+                                 span={4}>
+                                <Typography>
+                                    <Title level={4}>Status: {this.state.status.toUpperCase()}</Title>
+                                    <Title>
+                                        <Button type="primary" icon={<DownloadOutlined/>} size='large'>Download
+                                            Tutorial</Button>
+
+                                    </Title>
+                                </Typography>
+                            </Col>
+
+                            <Col style={{display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}
+                                 span={8}>
+                                <Typography>
+                                    <Title level={3}>{this.state.tutorial_name}</Title>
+                                    <Title level={4}>{this.state.foss}</Title>
+                                </Typography>
+                            </Col>
+
+                            <Col style={{display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}
+                                 span={8}>
+
+                                <ReactPlayer
+                                    height={200}
+                                    url={this.state.processed_video} controls/>
+                            </Col>
+
+                        </Row>
+                        <Row>
+                            <Table
+                                loading={this.state.isLoading}
+                                dataSource={this.state.chunks}
+                                columns={this.columns}/>
+                        </Row>
+                        <Modal
+                            title="Change Audio"
+                            visible={this.state.visible}
+                            onOk={this.handleOk}
+                            onCancel={this.handleCancel}
+                        >
+                            <Dragger {...this.config}>
+                                <p className="ant-upload-drag-icon">
+                                    <InboxOutlined/>
+                                </p>
+                                <p className="ant-upload-text">Click or drag audio to this area to upload</p>
+                            </Dragger>
+                        </Modal>
+                    </div>}
+
+            </div>)
     }
 
 
